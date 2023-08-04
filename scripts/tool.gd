96f7568fcb11d737cfa346f9d8a51f8e5de4c5db
erioxis <erioxis@vk.com>
2023-08-04 14:40:38 +0300

    tool retool

--- scripts/tool.gd
+++ scripts/tool.gd
@@ -1,6 +1,19 @@
-extends Node3D
+extends RigidBody3D
 
 class_name Tool
 
+var pickuped: bool
+@onready var coll = $CollisionShape3D
+
+func _ready():
+	pickuped = false
+
 func use():
+	pass
+	
+func pickup():
+	coll.disabled = true
+	freeze = true
+	
+func drop():
 	pass
