9e742900f4ab34e5563392bfabafedac236a9bb8
erioxis <erioxis@vk.com>
2023-08-06 11:47:54 +0300

    prop parent

--- scripts/prop.gd
+++ scripts/prop.gd
@@ -5,3 +5,8 @@ class_name Prop
 var health: float
 var durability: float
 var nails: int
+
+func _ready():
+	health = mass * global_transform.basis.x.length() * global_transform.basis.y.length() * global_transform.basis.z.length() / 10
+	durability = health * 3
+	nails = 0
--- tools/glock.gd
+++ tools/glock.gd
@@ -1,17 +1,18 @@
 extends Tool
-var amy = preload("res://Scenes/amyth.tscn")
-@onready var camera = $Head/Camera3D
-@onready var nextshoot = 0
+#var amy = preload("res://Scenes/amyth.tscn")
+#@onready var camera = $Head/Camera3D
+#@onready var nextshoot = 0
 @rpc("call_local","any_peer")
 func shoot():
-	nextshoot += 1
-	print(nextshoot)
-	if nextshoot > 5:
-		print("a")
-		var ameth = amy.instantiate()
-		Utils.world.add_child(ameth, true)
-		ameth.global_position = global_position
-		var dir:Vector3 = global_position - camera.global_position
-		var dist:float = dir.length()
-		ameth.apply_central_impulse(dir*dist*50)
-		nextshoot = 0
+	#nextshoot += 1
+	#print(nextshoot)
+	#if nextshoot > 5:
+		#print("a")
+		#var ameth = amy.instantiate()
+		#Utils.world.add_child(ameth, true)
+		#ameth.global_position = global_position
+		#var dir:Vector3 = global_position - camera.global_position
+		#var dist:float = dir.length()
+		#ameth.apply_central_impulse(dir*dist*50)
+		#nextshoot = 0
+	pass
