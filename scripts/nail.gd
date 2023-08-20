6742ac32f6acb401023fdb90ceb3b6e610f58979
erioxis <erioxis@vk.com>
2023-08-20 11:40:42 +0300

    bonk

--- scripts/nail.gd
+++ scripts/nail.gd
@@ -3,7 +3,11 @@ extends Node3D
 class_name Nail
 
 @onready var joint = $Generic6DOFJoint3D
+@onready var sound = $Sound
 
 func attach(obj1, obj2):
 	joint.node_a = obj1
 	joint.node_b = obj2
+
+func _ready():
+	sound.play()
